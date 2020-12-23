a0298261dac40bd6d8a26fa68a2346e8e5af55c8
Edson Elmar Schlei <edson.schlei@gmail.com>
2020-12-23 19:04:42 +0100

    Use custom font and add paddle and ball
    - Use custom font from file
    - Add 2 paddles, one left and one right
    - Add a ball in the middle of the screen

--- main.lua
+++ main.lua
@@ -5,8 +5,24 @@ local WINDOW_HEIGHT = 720
 local VIRTUAL_WIDTH = 432
 local VIRTUAL_HEIGHT = 243
 
-local MIDDLE_X = 0
-local MIDDLE_Y = 0
+local GAME_NAME_POSITION_X = 0
+local GAME_NAME_POSITION_Y = 0
+
+local GAME_AREA_Y = 20
+local GAME_AREA_X = 5
+local GAME_AREA_WIDTH = VIRTUAL_WIDTH - (GAME_AREA_X * 2)
+local GAME_AREA_HEIGHT = VIRTUAL_HEIGHT - (GAME_AREA_Y * 2)
+
+local PADDLE_WIDTH = 10
+local PADDLE_HEIGHT = 30
+
+local MAX_PADDLE_Y = VIRTUAL_HEIGHT - (GAME_AREA_Y + PADDLE_HEIGHT)
+local MIN_PADDLE_Y = GAME_AREA_Y
+
+local LEFT_PADDLE_X = GAME_AREA_X
+local RIGHT_PADDLE_X = VIRTUAL_WIDTH - (GAME_AREA_X + PADDLE_WIDTH)
+
+local BALL_SIZE = 20
 
 local push = require 'push'
 
@@ -21,20 +37,48 @@ function love.load()
         resizable = false
     })
 
+    local smallFont = love.graphics.newFont('04B03.TTF', 8)
+    love.graphics.setFont(smallFont)
+
     local font = love.graphics.getFont( )
     local hello_pong = love.graphics.newText(font, "Hello Pong!")
     local hp_tw_half = hello_pong:getWidth() / 2
     print(hp_tw_half)
-    MIDDLE_X = VIRTUAL_WIDTH / 2 - hp_tw_half
-    MIDDLE_Y = VIRTUAL_HEIGHT / 2 - 6
+    GAME_NAME_POSITION_X = VIRTUAL_WIDTH / 2 - hp_tw_half
+    GAME_NAME_POSITION_Y = 7
 end
 
+--[[
+    Draw the elements on the screen.
+]]
 function love.draw()
     push:apply('start')
-    love.graphics.print("Hello Pong!", MIDDLE_X, MIDDLE_Y)
+
+    -- it clears the screen with the defined collor
+    love.graphics.clear(40 / 255, 45 / 255, 52 / 255, 255 / 255)
+
+    -- it shows the name of the game
+    love.graphics.print("Hello Pong!", GAME_NAME_POSITION_X, GAME_NAME_POSITION_Y)
+
+    -- it draws the pong ball
+    local half_ball = BALL_SIZE / 2
+    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - half_ball, VIRTUAL_HEIGHT / 2 - half_ball, BALL_SIZE, BALL_SIZE)
+
+    -- it draws the fame area
+    love.graphics.rectangle('line', GAME_AREA_X , GAME_AREA_Y, GAME_AREA_WIDTH, GAME_AREA_HEIGHT)
+
+    -- it draws the left padle
+    love.graphics.rectangle('fill', LEFT_PADDLE_X, MIN_PADDLE_Y, PADDLE_WIDTH, PADDLE_HEIGHT)
+
+    -- it draws the right padle
+    love.graphics.rectangle('fill', RIGHT_PADDLE_X, MAX_PADDLE_Y, PADDLE_WIDTH, PADDLE_HEIGHT)
+
     push:apply('end')
 end
 
+--[[
+    catch the key events
+]]
 function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
